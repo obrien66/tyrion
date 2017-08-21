4f60ac2887b85c6f8bcf8e2e3d36386081590c9a
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-08-21 11:18:36 -0400

    IT WORKS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var html = function(place){
 		<!DOCTYPE html>
 		<html>
 			<head>
-			<title>Redirect</title>
+			<title>${place}</title>
 		</head>
 		<body>
 		Redirecting to ${place}
@@ -27,7 +27,7 @@ var html = function(place){
 }
 // HTML for failure
 var fail = function(code, message){
-	return `<!doctype html><html><head><title>${code}</title></head><body>${code}: ${message}</body></html>`
+	return `<!doctype html><html><head><title>${code}</title></head><body><h1>${code}: ${message}</h1></body></html>`
 }
 // Time for CLI
 var time = function(){
@@ -48,9 +48,13 @@ var time = function(){
 }
 
 http.createServer((req, res) => {
+	// Show time, http method, and requested URL
 	log(chalk.dim(time()) + " "+ chalk.blue(req.method) + " " + chalk.green(req.url));
+	// If it's a get request
 	if (req.method == "GET") {
+		// And if it's to the root directory
 		if (req.url == "/") {
+			// Show the welcome and available shortened links
 			res.writeHead(200, {"content-type": "text/html"})
 			res.write("<h1>Here are the urls that have been shortened</h1><a href='/submit'>Submit a new one</a><ul style='list-style-type:none;'>")
 			for (var i = 0; i < place.length; i++) {
@@ -58,52 +62,72 @@ http.createServer((req, res) => {
 			}
 			res.end("</ul>")
 		}
+		// If the users requests the "submit new url" page
 		else if (req.url == "/submit"){
+			// Read submit page
 			fs.readFile("submit.html", (err, data) => {
-				res.writeHead(404, "File Not Found", {"content-type": "text/html"})
-				if (err) res.end(fail("404", "File Not Found"))
+				// if theres and error send 404 error and 404 page
+				if (err){
+					res.writeHead(404, "File Not Found", {"content-type": "text/html"})
+					res.end(fail("404", "File Not Found"))
+				}
+				// Otherwise send a 200 response and the page
 				else  {
+					res.writeHead(200, {"content-type": "text/html"})
 					res.end(data)
 				}
 			})
 		}
 		else {
+			// Cut out the / of the requested url
 			let requested = req.url.substring(1)
+			// Use that to find the shortened url that the user wants
 			let find = links.indexOf(requested)
+			// if it's found
 			if (find >= 0) {
+				// Serve it to them hot and fresh using the html redirect function above
 				res.end(html(place[find]))
 			}
 			else {
+				// If the url isn't one I have send them the page to show them they messed up
 				fs.readFile("goof.html", "utf8", (err, data) => {
 					if (err){
+						// If there's an error send 404 response and 404 page
 						res.writeHead(404, "File Not Found", {"content-type": "text/html"})
 						res.end(fail("404", "File Not Found"))
 					}
+					// Otherwise it's gooda and send the page
 					else {
+						res.writeHead(200, {"content-type": "text/html"})
 						res.end(data)
 					}
 				});
 			}
 		}
 	}
+	// If the method is post
 	else if (req.method == "POST") {
-		if (req.method == "/send") {
-			let body = ""
-			req.on("data", (data) => {
-				log("Post working...");
-				body += data
-				if(body.length > 1e6) {req.connection.destroy()}
-			})
-			req.on("end", () => {
-				log("Post Worked!");
-				let formData = qs.parse(body)
-				links.push(formData.ext)
-				place.push(formData.url)
-				res.writeHead(200, {"content-type": "text/html"})
-				res.end(`Thanks! <a href="${formData.ext}">http://localhost:8000/${formData.ext}</a> will point to <a href="${formData.url}">${formData.url}</a>`)
-			})
-		}
+		let body = ""
+		// Take chuncks and save them in data
+		req.on("data", (data) => {
+			if(body.length > 1e6) {req.connection.destroy()}
+			// log("Post working...")
+			body += data
+		})
+		// When we have the data
+		req.on("end", () => {
+			// log("Post Worked!")
+			// Parse the query string
+			let formData = qs.parse(body)
+			// Save the link and the extension
+			links.push(formData.ext)
+			place.push(formData.url)
+			// Respond nicely
+			res.writeHead(200, {"content-type": "text/html"})
+			res.end(`Thanks! <a href="${formData.ext}">http://localhost:8000/${formData.ext}</a> will point to <a href="${formData.url}">${formData.url}</a><br><br><a href="/">home</a>`)
+		})
 	}
+	// If they use a weird method say no thanks
 	else {
 		res.writeHead(405, "Method Not Supported", {"content-type": "text/html"})
 		res.end(fail("405", "Method Not Supported"))
